802dffac94407b56626310957c52e9b9475462a2
Arka Saha <i.am.arka.saha@gmail.com>
2025-02-08 18:18:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-let txt = document.querySelector("#head");
-const messages = ["happiieeee", "rosseey 🌹", "daayyy naa", "shhh", "my", "beautiffulllest 🥺✨", "rooseey bubuu ❤️"];
+let head = document.querySelector(".head");
+let ringtxt = document.querySelector(".ring-txt");
 
+let head_arr1 = ["bubuududu na,", "happieiest", "proposeyy 🥺", "dayyy", "okie na 🫶", "shhhh hehe", "❤️🥺"];
+let ring_arr1 = ["sweetheart", "youuu", "are", "mah", "forebahh na 🌹", "I love you bubu 🥺"]
 
+let headarr2 = ["babyy", "will", "you", "be", "m-my", "valentinee?", "🥺 hehe", "Will you be my valentine baby? 🥺❤️"];
 let i = 0;
-let intv = setInterval(() => {
-    if(i<messages.length)
+let intv1 = setInterval(() => {
+
+    if(i!= head_arr1.length)
     {
-        txt.textContent = messages[i++];
+        head.textContent = head_arr1[i];
+        i++;
     }else{
-        clearInterval(intv);
-    }
+    clearInterval(intv1);}
+    
     
 }, 1000);
+
+setTimeout(() => {
+    let j=0;
+    let intv2 = setInterval(() => {
+        if(j!= ring_arr1.length)
+            {
+                ringtxt.textContent = ring_arr1[j];
+                j++;
+            }else{
+            clearInterval(intv2);}
+    }, 1000);
+
+}, 8500);
+
+setTimeout(() => {
+   
+    let a = 0;
+    let intv3 = setInterval(() => {
+        if(a!= headarr2.length)
+            {
+                head.textContent = headarr2[a];
+                a++;
+            }else{
+            clearInterval(intv3);}
+    }, 1000);
+
+}, 15000);
